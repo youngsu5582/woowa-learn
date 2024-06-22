486dea5c7bd8d735dcea790b4a5a2a58b25b063f
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-06-22 13:48:32 +0900

    feat: 인수 테스트 초안 작성

---
+++ src/test/java/org/example/woowalearn/acceptance/config/AuthAcceptanceTest.java
@@ -0,0 +1,25 @@
+package org.example.woowalearn.acceptance.config;
+
+import org.junit.jupiter.api.Test;
+
+@AcceptanceTest
+class AuthAcceptanceTest {
+    @Test
+    void 회원가입_성공_플로우(){
+        //이메일,비밀번호를 입력한다.
+
+        //회원가입을 진행한다.
+
+        //회원가입을 성공한다.
+    }
+    @Test
+    void 중복_이메일이_있을시_실패한다(){
+        //이메일,비밀번호를 입력한다.
+
+        //회원가입을 진행한다.
+
+        //같은 이메일로 회원가입을 한번더 진행한다.
+
+        //회원가입을 실패한다.
+    }
+}
