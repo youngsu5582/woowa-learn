b27432005e8c651b3a2e8034e355f5a620086ea0
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-06-24 18:03:07 +0900

    feat: 토큰 검증,예외 필터 구현

---
+++ src/main/java/org/example/woowalearn/global/config/security/JwtAuthenticationFilter.java
@@ -0,0 +1,45 @@
+package org.example.woowalearn.global.config.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.example.woowalearn.user.service.JwtProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends GenericFilterBean {
+
+    private final JwtProvider jwtProvider;
+
+    public JwtAuthenticationFilter(final JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
+            throws ServletException, IOException {
+        final String token = resolveToken((HttpServletRequest)request);
+
+        if (token != null) {
+            jwtProvider.validateToken(token);
+            final Authentication authentication = jwtProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    private String resolveToken(final HttpServletRequest request) {
+        final String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
