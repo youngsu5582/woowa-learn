e820434f3813307721168748679bcc7e9c1c8fd8
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-06-24 14:40:08 +0900

    feat: 로그인에 대한 인수 테스트 사전 정의

--- src/test/java/org/example/woowalearn/acceptance/AuthAcceptanceTest.java
+++ src/test/java/org/example/woowalearn/acceptance/AuthAcceptanceTest.java
@@ -1,6 +1,8 @@
 package org.example.woowalearn.acceptance;
 
 import org.example.woowalearn.acceptance.config.AcceptanceTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.example.woowalearn.acceptance.step.given.AuthGivenStep.회원가입_정보_생성;
@@ -15,32 +17,65 @@ class AuthAcceptanceTest {
     private final String 비밀번호 = "password1234";
     private final boolean 이메일_수신_동의 = true;
 
-    @Test
-    void 회원가입을_성공한다() {
-        final var 회원가입_사용자_정보 = 회원가입_정보_생성(
-                이메일,
-                비밀번호,
-                이메일_수신_동의
-        );
+    @Nested
+    @DisplayName("회원가입")
+    class RegisterCase{
+        @Test
+        void 회원가입을_성공한다() {
+            final var 회원가입_사용자_정보 = 회원가입_정보_생성(
+                    이메일,
+                    비밀번호,
+                    이메일_수신_동의
+            );
 
-        final var 회원가입_결과 = 회원가입(회원가입_사용자_정보);
+            final var 회원가입_결과 = 회원가입(회원가입_사용자_정보);
 
-        생성_성공한지_검증(회원가입_결과);
-        회원가입_성공한지_검증(회원가입_결과);
+            생성_성공한지_검증(회원가입_결과);
+            회원가입_성공한지_검증(회원가입_결과);
+        }
+
+        @Test
+        void 중복_이메일이_있을시_실패한다() {
+            final var 회원가입_사용자_정보 = 회원가입_정보_생성(
+                    이메일,
+                    비밀번호,
+                    이메일_수신_동의
+            );
+
+            final var 회원가입_결과 = 회원가입(회원가입_사용자_정보);
+            회원가입_성공한지_검증(회원가입_결과);
+
+            final var 중복_이메일_회원가입_결과 = 회원가입(회원가입_사용자_정보);
+            잘못된_요청인지_검증(중복_이메일_회원가입_결과);
+        }
+    }
+    @Nested
+    @DisplayName("로그인")
+    class LoginCase{
+        @Test
+        void 로그인을_성공한다(){
+            // 회원가입
+
+            // 로그인
+
+            // 성공
+        }
+        @Test
+        void 없는_이메일로_로그인_할_시_실패한다(){
+            // 회원가입
+
+            // 없는 이메일로 로그인
+
+            // 실패
+        }
+        @Test
+        void 비밀번호가_틀리면_실패한다(){
+            // 회원가입
+
+            // 다른 비밀번호로 로그인
+
+            // 실패
+        }
     }
 
-    @Test
-    void 중복_이메일이_있을시_실패한다() {
-        final var 회원가입_사용자_정보 = 회원가입_정보_생성(
-                이메일,
-                비밀번호,
-                이메일_수신_동의
-        );
-
-        final var 회원가입_결과 = 회원가입(회원가입_사용자_정보);
-        회원가입_성공한지_검증(회원가입_결과);
-
-        final var 중복_이메일_회원가입_결과 = 회원가입(회원가입_사용자_정보);
-        잘못된_요청인지_검증(중복_이메일_회원가입_결과);
-    }
 }
