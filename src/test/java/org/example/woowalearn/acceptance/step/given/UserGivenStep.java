0e9e1f01c212ef7fca9647647e027ed1425d40d4
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-06-28 00:50:54 +0900

    feat: /user/apply 에 자신의 지원서 받아오는 기능 연결

--- src/test/java/org/example/woowalearn/acceptance/step/given/UserGivenStep.java
+++ src/test/java/org/example/woowalearn/acceptance/step/given/UserGivenStep.java
@@ -1,6 +1,9 @@
 package org.example.woowalearn.acceptance.step.given;
 
+import org.example.woowalearn.user.dto.ApplyResponse;
 import org.example.woowalearn.user.dto.ApplyTeacherRequest;
+
+import static org.example.woowalearn.acceptance.step.when.UserWhenStep.본인_지원서_조회;
 
 public class UserGivenStep {
     public static ApplyTeacherRequest 교육자_지원서_정보_성성(final String applyReason, final String phoneNumber, final String explainLink, final String contractName, final String contactEmail) {
@@ -11,5 +14,10 @@ public class UserGivenStep {
                 contractName,
                 contactEmail
         );
+    }
+
+    public static ApplyResponse 본인_지원서_결과_조회(final String accessToken) {
+        return 본인_지원서_조회(accessToken).extract()
+                .as(ApplyResponse.class);
     }
 }
