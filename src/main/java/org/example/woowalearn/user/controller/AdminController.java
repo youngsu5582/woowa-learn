8e931928d1e38ff340f0f07134d4f417feef0047
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-06-28 10:59:28 +0900

    feat: AdminController 에 이메일 전송 기능 추가

--- src/main/java/org/example/woowalearn/user/controller/AdminController.java
+++ src/main/java/org/example/woowalearn/user/controller/AdminController.java
@@ -3,7 +3,9 @@ package org.example.woowalearn.user.controller;
 import lombok.AllArgsConstructor;
 import org.example.woowalearn.user.domain.UserPrincipal;
 import org.example.woowalearn.user.dto.ApplyChangeRequest;
+import org.example.woowalearn.user.dto.ApplyResponse;
 import org.example.woowalearn.user.service.ApplyService;
+import org.example.woowalearn.user.service.EmailSender;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -13,17 +15,20 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class AdminController {
     private final ApplyService applyService;
+    private final EmailSender emailSender;
 
     @PostMapping("/apply/approve")
     public ResponseEntity<Void> approveApply(@AuthenticationPrincipal final UserPrincipal userPrincipal, @RequestBody final ApplyChangeRequest request) {
-        applyService.approveApply(userPrincipal.getUserId(), request);
+        final ApplyResponse result = applyService.approveApply(userPrincipal.getUserId(), request);
+        emailSender.sendApplyJudgeMail(result.contractName(),result.contactEmail(),result.applyStatus());
         return ResponseEntity.ok()
                 .build();
     }
 
     @PostMapping("/apply/deny")
     public ResponseEntity<Void> approveDeny(@AuthenticationPrincipal final UserPrincipal userPrincipal, @RequestBody final ApplyChangeRequest request) {
-        applyService.denyApply(userPrincipal.getUserId(), request);
+        final ApplyResponse result = applyService.denyApply(userPrincipal.getUserId(), request);
+        emailSender.sendApplyJudgeMail(result.contractName(),result.contactEmail(),result.applyStatus());
         return ResponseEntity.ok()
                 .build();
     }
--- src/test/java/org/example/woowalearn/acceptance/step/then/CommonThenStep.java
+++ src/test/java/org/example/woowalearn/acceptance/step/then/CommonThenStep.java
@@ -1,6 +1,8 @@
 package org.example.woowalearn.acceptance.step.then;
 
 import io.restassured.response.ValidatableResponse;
+import org.example.woowalearn.user.service.EmailSender;
+import org.mockito.Mockito;
 
 public class CommonThenStep {
     public static void 생성_성공한지_검증(final ValidatableResponse response) {
@@ -16,5 +18,10 @@ public class CommonThenStep {
     public static void 잘못된_요청인지_검증(final ValidatableResponse response) {
         response.assertThat()
                 .statusCode(400);
+    }
+
+    public static void 메일_발송한지_검증(final String email, final String contractName, final EmailSender emailSender,final String applyStatus) {
+        Mockito.verify(emailSender,Mockito.times(1))
+                .sendApplyJudgeMail(contractName, email,applyStatus);
     }
 }
