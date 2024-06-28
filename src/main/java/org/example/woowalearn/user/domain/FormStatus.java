74aff686b1352f7effe13f3f8e00a303f2f08a46
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-06-28 10:57:22 +0900

    feat: 이메일 전송 기능 구현

--- src/main/java/org/example/woowalearn/user/domain/FormStatus.java
+++ src/main/java/org/example/woowalearn/user/domain/FormStatus.java
@@ -11,4 +11,13 @@ public enum FormStatus {
     FormStatus(final String value) {
         this.value = value;
     }
+
+    public static FormStatus from(final String value) {
+        for (final FormStatus formStatus : FormStatus.values()) {
+            if (formStatus.value.equals(value)) {
+                return formStatus;
+            }
+        }
+        throw new IllegalArgumentException(String.format("%s는 없는 값입니다.",value));
+    }
 }
